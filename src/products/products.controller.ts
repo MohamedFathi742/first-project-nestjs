2b08513ec538aa8e0ddd085942f3f22ef47e0a02
MohamedFathi742 <mofathi742002@gmail.com>
2025-10-28 11:48:45 +0300

    Implement ProductsService with product data and findAll method; add getAllProducts endpoint in ProductsController

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,7 +1,13 @@
 import { Controller } from '@nestjs/common';
 import { ProductsService } from './products.service';
+import { get } from 'http';
+import { Product } from './interfaces/product.interface';
 
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
+  @Get()
+  getAllProducts():Product[] {
+    return this.productsService.findAll();
+  }
 }
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,53 @@
 import { Injectable } from '@nestjs/common';
+import { Product } from './interfaces/product.interface';
 
 @Injectable()
-export class ProductsService {}
+export class ProductsService {
+
+private products:Product[] = [
+{
+
+id: '1',
+  name: 'Product 1',
+  price: 100,
+  description: 'Description for Product 1',
+  stock: 10,
+  category: 'Category A',
+  isActive: true,
+  createdAt: new Date(),
+  updatedAt: new Date(),
+
+
+}
+{
+
+id: '2',
+  name: 'Product 2',
+  price: 100,
+  description: 'Description for Product 2',
+  stock: 10,
+  category: 'Category b',
+  isActive: true,
+  createdAt: new Date(),
+  updatedAt: new Date(),
+
+
+}
+
+findAll(): Product[] {
+    return this.products;
+  }
+
+
+];
+
+
+
+
+
+
+
+
+
+
+}
